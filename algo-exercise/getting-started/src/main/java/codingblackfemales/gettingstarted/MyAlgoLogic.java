793675c6f15ded5c9ab2ff3631b1f6d1e01dff6e
Shola Abhulimhen <sholaabhus@gmail.com>
2024-09-16 20:21:11 +0000

    Cancel-Buy-order

--- algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
+++ algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
@@ -1,12 +1,11 @@
 package codingblackfemales.gettingstarted;
 
-import codingblackfemales.action.Action;
-import codingblackfemales.action.CancelChildOrder;
-import codingblackfemales.action.CreateChildOrder;
-import codingblackfemales.action.NoAction;
+import codingblackfemales.action.*;
 import codingblackfemales.algo.AlgoLogic;
 import codingblackfemales.marketdata.gen.RandomMarketDataGenerator;
+import codingblackfemales.orderbook.OrderBook;
 import codingblackfemales.orderbook.channel.MarketDataChannel;
+import codingblackfemales.orderbook.order.Order;
 import codingblackfemales.service.MarketDataService;
 import codingblackfemales.service.OrderService;
 import codingblackfemales.sotw.ChildOrder;
@@ -20,6 +19,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.swing.plaf.nimbus.State;
+import java.util.stream.Collectors;
 
 public class MyAlgoLogic implements AlgoLogic {
 
@@ -33,15 +33,21 @@ public class MyAlgoLogic implements AlgoLogic {
         var orderBookAsString = Util.orderBookToString(state);
 
         logger.info("[MYALGO] The state of the order book is:\n" + orderBookAsString);
-
+        logger.info("########### CURRENT ORDERS: " + state.getActiveChildOrders().toString());
+        var totalOrderCount = state.getChildOrders().size();
+        //make sure we have an exit condition...
+        if (totalOrderCount > 10) {
+            return NoAction.NoAction;
+        }
 
         final BidLevel bidLevel = state.getBidAt(0);
         final long bidPrice = bidLevel.price;
         final long bidQuantity = bidLevel.quantity;
+        final long bestBidPrice = state.getBidAt(0).price; // get the current bidprice
 
-        final AskLevel askLevel = state.getAskAt(0);
-        final long askPrice = askLevel.price;
-        final long askQuantity = askLevel.quantity;
+//        final AskLevel askLevel = state.getAskAt(0);
+//        final long askPrice = askLevel.price;
+//        final long askQuantity = askLevel.quantity;
 
         //capture current size of order before creating new one
         // int currentOrderSize = state.getChildOrders().size();// capture current size of order before creating new one
@@ -49,26 +55,32 @@ public class MyAlgoLogic implements AlgoLogic {
 
         // seperate childorder into BUY AND SELL
         long buyOrdersCount = state.getChildOrders().stream().filter(ChildOrder -> ChildOrder.getSide() == Side.BUY).count();
-        long sellOrdersCount = state.getChildOrders().stream().filter(ChildOrder -> ChildOrder.getSide() == Side.SELL).count();
+       // long sellOrdersCount = state.getChildOrders().stream().filter(ChildOrder -> ChildOrder.getSide() == Side.SELL).count();
 
 
-        //check if there are few than 5 child orders
-       // if (state.getChildOrders().size() < 5) {
-            if (buyOrdersCount < 4) { // create new Buy order if fewer than 5 child order exist
+            if (buyOrdersCount < 1) { // create new Buy order
                 // If less than 4 BUY orders, create a new BUY order
                 logger.info("[PASSIVEALGO] Have: " + buyOrdersCount + " orders. ADD New BUY ORDER " + bidQuantity + " @ " + bidPrice);
                 //  int initialOrderSize = state.getChildOrders().size();// capture current size of order before creating new one
                 return new CreateChildOrder(Side.BUY, bidQuantity, bidPrice); // Create a new BUY order
             }
- //If no actions are taken, return NoAction
-           //   return NoAction.NoAction;
-           // }
 
-            if (sellOrdersCount < 3) { //create new sell order if fewer than 5 child order exist
-              logger.info("[PASSIVEALGO] Ha: " + sellOrdersCount + " orders. ADD New ASK ORDER " + askQuantity + " @ " + askPrice);
-            //int initialOrderSize = state.getChildOrders().size();// capture current size of order before creating new one
-              return new CreateChildOrder(Side.SELL, askQuantity, askPrice); // Create a new child order
-               }
+        // Cancel the  First  BUY order with a price less than the BestBid or not matching the Bestbid.
+            for(ChildOrder order: state.getActiveChildOrders()) {
+                if (order.getSide() == Side.BUY && (order.getPrice() != bestBidPrice || order.getPrice() < bestBidPrice)) {
+                    logger.info(String.format("[MYALGO] Cancel BUY order #%d with price %d and quantity %d. The current best price is %d.",order.getOrderId(),order.getPrice(), order.getQuantity(),bestBidPrice));
+                    logger.info(state.getActiveChildOrders().toString());
+                    return new CancelChildOrder(order);
+
+                }
+
+            }
+
+//            if (sellOrdersCount < 3) { //create new sell order if fewer than 5 child order exist
+//              logger.info("[PASSIVEALGO] Ha: " + sellOrdersCount + " orders. ADD New ASK ORDER " + askQuantity + " @ " + askPrice);
+//            //int initialOrderSize = state.getChildOrders().size();// capture current size of order before creating new one
+//              return new CreateChildOrder(Side.SELL, askQuantity, askPrice); // Create a new child order
+//               }
 
 // If no actions are taken, return NoAction
             return NoAction.NoAction;
