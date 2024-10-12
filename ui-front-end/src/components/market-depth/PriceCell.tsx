39aeee31972ec27442e37af25f06ed0ddf2a6817
Shola Abhulimhen <sholaabhus@gmail.com>
2024-10-12 17:48:56 +0000

    add-dependency

--- ui-front-end/src/components/market-depth/PriceCell.tsx
+++ ui-front-end/src/components/market-depth/PriceCell.tsx
@@ -6,14 +6,15 @@ export const PriceCell =(props: PriceCellProps)=>{
     const lastPriceRef = useRef(props.price);
     //const [arrowPosition, setArrowPosition] =useState(0);
     const diff = props.price - lastPriceRef.current;
+    
     useEffect(() =>{
-  lastPriceRef.current =props.price;
+  //lastPriceRef.current =props.price;
     // Log the price difference for debugging
             console.log(`Price: ${props.price}, Last Price: ${lastPriceRef.current}, Diff: ${diff}`);
 
 //update the reference after the log
 lastPriceRef.current =props.price;
-    }, [props.price]); // run when price changes
+    }, [diff, props.price]); // run when price changes
 
 return (
         <div className="price-cell-container" style={{ display: 'flex', alignItems: 'center' }}>
